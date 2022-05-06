d0587cdea278d26e670cab781af49efe862c2bc0
Aoi Ohtani <a.otani.252@stn.nitech.ac.jp>
2022-05-06 23:58:48 +0900

    expander

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ button = left_column.button('右カラムに文字表示')
 if button:
     right_column.write('ここは右カラム')
 
+expaner1 = st.expander('問い合わせ1')
+expaner1.write('問い合わせ1の回答')
+expaner2 = st.expander('問い合わせ2')
+expaner2.write('問い合わせ2の回答')
+expaner3 = st.expander('問い合わせ3')
+expaner3.write('問い合わせ3の回答')
 
 # text = st.text_input('あなたの趣味を教えてください。',)
 # conditon = st.slider('あなたの今の調子は？', 0, 100, 50)
