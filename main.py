43dd0122a8e779435e9199dfc959c5fdc8a994ff
Aoi Ohtani <a.otani.252@stn.nitech.ac.jp>
2022-05-06 23:41:42 +0900

    Interractive Widget

---
+++ main.py
@@ -0,0 +1,14 @@
+from matplotlib.pyplot import axis
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+
+st.title('Streamlit 超入門')
+
+st.write('Interractive Widgets')
+
+text = st.text_input('あなたの趣味を教えてください。',)
+
+'あなたの趣味：', text
