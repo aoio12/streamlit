68ab094b051b1dbe5269d971264775a4388acebe
Aoi Ohtani <a.otani.252@stn.nitech.ac.jp>
2022-05-07 00:07:59 +0900

    progress bar

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from PIL import Image
+import time
 
 
 st.title('Streamlit 超入門')
 
-st.write('Interractive Widgets')
+st.write('プログレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i + 1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'Done!!!'
 
 left_column, right_column = st.columns(2)
-
 button = left_column.button('右カラムに文字表示')
 if button:
     right_column.write('ここは右カラム')
